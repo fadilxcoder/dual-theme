6707999e9c0d9a4fdd0357225ca58d1dd398ea0a
Fadil <rosunmungurudm@gmail.com>
2020-12-20 11:54:40 +0400

    git hook - auto-deploy - 22

--- script.js
+++ script.js
@@ -38,4 +38,4 @@ function toggleTheme(e)
 
 document.getElementById('toggler').addEventListener('click', toggleTheme);
 
-console.log('git automated-deploy - private repo & just push event');
+console.log('git automated-deploy -testing wget');
